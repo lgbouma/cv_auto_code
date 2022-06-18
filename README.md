1869a7cbf23402c207f830e67212d0fb76276309
Kris Pardo <kpardo@caltech.edu>
2022-06-17 18:11:48 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+This script creates tex files with lists of publications from ADS libraries. The lists will have your name bolded and include links to the ADS pages for each of your papers.
+
+To use, you will first need to edit three files:
+
+1. `my_key.txt` -- your [https://ui.adsabs.harvard.edu/user/settings/token][ADS API key]. You need this to access the API, even for public libraries.
+2. `my_names.txt` -- how your name is listed in bibtex entries. This will then enable the script to bold your name. Typically this is in the form "{LastName}, FirstName". Put only one name on each line.
+3. `my_libraries.txt` -- each line should have an ADS library id, followed by the latex file you wish to output to.
+
+This will create as many tex files as lines in `my_libraries.txt`. You can then use the `\input{}` command in your CV tex file to put these files within your cv.
+
+**Pro-tip** - You can run this script every time you compile your CV by doing the following:
+1. Before `\begin{document}` in your CV tex file, insert `\immediate\write18{./run_cv_gen.sh}`. (You might need to make the shell file executable)
+2. When you call pdflatex, use the flag `--shell-escape`.
---
+++ make_pub_lists.py
@@ -0,0 +1,68 @@
+## import all the things
+import numpy as np
+import requests
+import json
+import pandas as pd
+import bibtexparser
+import sys
+
+## get my token
+token = np.loadtxt("my_key.txt", dtype=str)
+token = str(token)
+
+## get my libraries
+libs, fns = np.loadtxt("my_libraries.txt", dtype=str, unpack=True)
+
+def get_df(lib):
+    ## get my papers from library
+    r = requests.get("https://api.adsabs.harvard.edu/v1/biblib/libraries/"+lib,\
+                    headers={'Authorization': 'Bearer ' + token})
+    docs = r.json()['documents']
+    ## get bibtex
+    payload = {"bibcode":docs}
+    r = requests.post("https://api.adsabs.harvard.edu/v1/export/bibtex", \
+                     headers={"Authorization": "Bearer " + token, "Content-type": "application/json"}, \
+                     data=json.dumps(payload))
+    bibtex = r.json()['export']
+    ## parse bibtex
+    parser = bibtexparser.bparser.BibTexParser(common_strings=True)
+    parser.ignore_nonstandard_types = False
+    parser.homogenise_fields = False
+    bib_database = bibtexparser.loads(bibtex, parser = parser)
+    ## get df
+    df = pd.DataFrame(bib_database.entries)
+    ## select needed columns
+    try:
+        selection = df[['author', 'title', 'journal', 'volume', 'number', 'eid', 'adsurl', 'primaryclass']]
+    except: ##above will throw error if only nonref papers
+            selection = df[['author', 'title', 'journal', 'eid', 'adsurl', 'primaryclass']]
+    return selection
+
+def fix_auth(entry):
+    text = entry['author'].replace(' and ', '; ')
+    names = np.loadtxt('my_names.txt', dtype=str, delimiter='\n')
+    for n in names:
+        text = text.replace(str(n), f'\\textbf{{{str(n)}}}')
+    return text
+
+def make_item(e):
+    auths = fix_auth(e)
+    title = f'\\textit{{{e["title"]}}}'
+    s = ', '
+    if e['journal'] == 'arXiv e-prints':
+        url = f'\\href{{{e["adsurl"]}}}{{{e["eid"]}}}'
+    else:
+        url = f'\\href{{{e["adsurl"]}}}{{{s.join([e["journal"], e["volume"], e["number"], e["eid"]])}}}'
+    if e['primaryclass'] == 'hep-ph':
+        final = '\\item $\\dagger$ '+ s.join([auths, title, url])
+    else:
+        final = '\\item '+ s.join([auths, title, url])
+    return final
+
+#### Run
+for i,l in enumerate(libs):
+    df = get_df(l)
+    with open(fns[i], 'w') as f:
+        for _,e in df.iterrows():
+            f.write(make_item(e)+'\n \n')
+    print(f'Wrote {fns[i]}. Has {len(df)} entries.')
